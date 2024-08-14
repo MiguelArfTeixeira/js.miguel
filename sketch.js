6cd15a6cc4d731656a088c0ec3ed5bf00b92254b
MiguelArfTeixeira <miguel.arf.teixeira@escola.pr.gov.br>
2024-08-14 14:03:01 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,28 @@
+let palavras;
+
+
+function inicializaCores(){
+ background("red");//cor de fundo
+ fill("yellow");//preenchimento
+ textSize(64);//tamamho da fonte
+  textAlign(CENTER, CENTER)//alinha o texto ao centro
+}  
+
+
+function setup() {
+  createCanvas(400, 400);
+  let palavras=['arf','miguel', 'lindo'];
+  palavra = random(palavras);
+              
+}
+
+function draw() {
+  
+  inicializaCores();
+  
+  
+  let quantidade = map(mouseX, 0, width, 0, palavra.length);
+  //let quantidaade=map(posição do mause,0,largura do canva,0,quantidade de letras da palavara);
+  let parcial = palavra.substring(0,quantidade);
+  text(parcial,200,200);
+}
